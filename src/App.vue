be05992c5f51fd01911c1bf24d0c6ac26dcac09d
adas0mahdi <156195433+mahdi0adas@users.noreply.github.com>
2024-06-02 05:02:53 +0800

    change color

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script setup>
 import { ref } from "vue";
 import emoji from "./components/emoji.vue";
-
+import InterchangeableColors from "./components/InterchangeableColors.vue";
 const showEmoji = ref(false);
 </script>
 
 <template>
   <div
-    class="flex justify-center items-center h-10 bg-green-300 text-gray-700 py-5"
+    class="flex justify-center items-center h-10 bg-green-300 text-white py-5"
   >
     <button @click="showEmoji = !showEmoji">Emoji</button>
   </div>
   <div v-if="showEmoji">
     <emoji />
+  </div>
+  <div>
+    <InterchangeableColors />
   </div>
 </template>
 
---
+++ src/components/InterchangeableColors.vue
@@ -0,0 +1,115 @@
+<script setup>
+import { ref, watch } from "vue";
+
+const background = ref("");
+// Watcher to log changes to background value
+watch(background, (newValue) => {
+  console.log("background", newValue);
+  document.body.style.backgroundColor = newValue;
+});
+
+// Method to change the background color
+const changeBackground = (color) => {
+  background.value = color;
+};
+</script>
+
+<template>
+  <div class="content-container">
+    <nav class="nav">
+      <input
+        type="button"
+        @click="changeBackground('purple')"
+        value="purple"
+        class="btn purple"
+      />
+      <input
+        type="button"
+        @click="changeBackground('blue')"
+        value="blue"
+        class="btn blue"
+      />
+      <input
+        type="button"
+        @click="changeBackground('red')"
+        value="red"
+        class="btn red"
+      />
+      <input
+        type="button"
+        @click="changeBackground('pink')"
+        value="pink"
+        class="btn pink"
+      />
+      <input
+        type="button"
+        @click="changeBackground('yellow')"
+        value="yellow"
+        class="btn yellow"
+      />
+      <input
+        type="button"
+        @click="changeBackground('black')"
+        value="black"
+        class="btn black"
+      />
+    </nav>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: rgb(28, 28, 35);
+}
+
+.btn {
+  padding: 10px 20px;
+  cursor: pointer;
+  border: none;
+  color: #fff;
+  border: 2px solid white;
+  width: 100px;
+  height: 100px;
+  margin: 20px;
+  border-radius: 100%;
+  text-transform: capitalize;
+}
+
+.purple {
+  background-color: purple;
+}
+
+.blue {
+  background-color: blue;
+}
+
+.red {
+  background-color: red;
+}
+
+.pink {
+  background-color: pink;
+}
+
+.yellow {
+  background-color: yellow;
+}
+
+.black {
+  background-color: black;
+}
+
+.content-container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
