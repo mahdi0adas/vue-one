21b49b8098b18c006244b88203718cc45cb13b4e
adas0mahdi <156195433+mahdi0adas@users.noreply.github.com>
2024-06-02 04:26:01 +0800

    x

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+import emoji from "./components/emoji.vue";
+
+const showEmoji = ref(false);
+</script>
+
+<template>
+  <div
+    class="flex justify-center items-center h-10 bg-green-300 text-gray-700 py-5"
+  >
+    <button @click="showEmoji = !showEmoji">Emoji</button>
+  </div>
+  <div v-if="showEmoji">
+    <emoji />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/emoji.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref } from "vue";
+
+const emojis = ref([
+  "😂",
+  "😘",
+  "😊",
+  "🤣",
+  "😍",
+  "😁",
+  "😢",
+  "😎",
+  "😉",
+  "🙂",
+  "🫡",
+  "😮",
+  "😥",
+]);
+const currentEmoji = ref(emojis.value[0]);
+
+const getRandomEmoji = () => {
+  const randomIndex = Math.floor(Math.random() * emojis.value.length);
+  currentEmoji.value = emojis.value[randomIndex];
+};
+</script>
+
+<template>
+  <div class="flex w-full h-full justify-center items-center">
+    <div
+      @mouseover="getRandomEmoji"
+      class="emoji big-box rounded-full mt-40 p-10 bg-slate-500"
+    >
+      <span>{{ currentEmoji }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.big-box {
+  width: 21%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.emoji {
+  font-size: 10rem;
+  filter: grayscale(1);
+  cursor: pointer;
+  transition-property: transform, filter;
+  transition-duration: 200ms;
+}
+
+.emoji:hover {
+  transform: scale(1.3);
+  filter: grayscale(0);
+}
+</style>
