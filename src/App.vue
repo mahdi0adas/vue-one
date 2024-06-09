677e8d6ed81029ce893087932e30ca0440b5c905
adas0mahdi <156195433+mahdi0adas@users.noreply.github.com>
2024-06-09 15:24:12 +0800

    s

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <script setup>
-import { ref , onMounted} from "vue";
+import { ref, onMounted } from "vue";
 import emoji from "./components/emoji.vue";
 import InterchangeableColors from "./components/InterchangeableColors.vue";
 import feedBack from "./components/feedBack.vue";
 import calculator from "./components/calculator.vue";
 import { gsap } from "gsap";
 
-
-
 const Emoji = ref(false);
 const Color = ref(false);
 const FeedBack = ref(false);
 const Calculator = ref(false);
-
 
 const textColor = ref("");
 
@@ -47,8 +44,6 @@ const handleColorChange = (color) => {
   textColor.value = color;
 };
 
-
-
 onMounted(() => {
   gsap.to(".box", {
     x: 2000,
@@ -62,17 +57,12 @@ onMounted(() => {
   <div
     class="flex gap-3 justify-center items-center h-[30px] bg-black text-white py-5"
   >
-    <button 
-    :style='[`color: ${textColor} ;`]' @click="showEmoji">
-      Emoji
-    </button>
-    <button :style='[`color: ${textColor} ;`]' @click="showColor">
-      Color
-    </button>
-    <button :style='[`color: ${textColor} ;`]' @click="showFeedBack">
+    <button :style="[`color: ${textColor} ;`]" @click="showEmoji">Emoji</button>
+    <button :style="[`color: ${textColor} ;`]" @click="showColor">Color</button>
+    <button :style="[`color: ${textColor} ;`]" @click="showFeedBack">
       Feed Back
     </button>
-    <button :style='[`color: ${textColor} ;`]' @click="showCalculator">
+    <button :style="[`color: ${textColor} ;`]" @click="showCalculator">
       calculator
     </button>
   </div>
@@ -80,7 +70,7 @@ onMounted(() => {
     <emoji />
   </div>
   <div v-else-if="Color">
-    <InterchangeableColors   @colorChange="handleColorChange" />
+    <InterchangeableColors @colorChange="handleColorChange" />
   </div>
   <div v-else-if="FeedBack">
     <feedBack />
@@ -93,14 +83,12 @@ onMounted(() => {
     class="flex text-white py-5 text-[50px] justify-center items-center h-[400px]"
   >
     Please choose something from the navBar!
-    </div>
-    <div class="bg-yellow-500 w-[50px] h-[50px] rounded-full box">
-    </div>
+  </div>
+  <div class="bg-yellow-500 w-[50px] h-[50px] rounded-full box"></div>
 </template>
 
 <style scoped>
 .btn {
   padding: 2px 20px;
-
 }
 </style>
